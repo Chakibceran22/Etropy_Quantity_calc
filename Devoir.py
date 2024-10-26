00c99ab570b60f66ecf849fb9badb1b68e5af573
Chakibceran22 <grabachakib008@gmail.com>
2024-10-26 17:48:58 +0100

    creating the first question with soem issues

---
+++ Devoir.py
@@ -0,0 +1,49 @@
+import math
+
+source = []
+probalblities = []
+inp = 0;
+calcInput = 0;
+while(inp != "#"):
+    inp = input("Entrez Les nombres de la source et pour arreter tapez #: ")
+    if(inp == '#'):
+      break
+    while True:
+       try:
+            prob = float(input("Entrez la probabilite de cet element (entre 0 et 1): "))
+            if 0 <= prob <= 1:
+                break
+            else:
+                print("La probabilite doit être entre 0 et 1. Veuillez réessayer.")
+       except ValueError:
+            print("Entrée invalide. Veuillez entrer un nombre.")
+    source.append(inp)
+    probalblities.append(float(prob))
+    print("La somme des probabilites est: ", sum(probalblities))
+
+
+        
+
+print("La source est: ", source)
+print("Les probabilites sont: ", probalblities)
+
+while True:
+    calcInput = input("Entrez 1 pour calculer la quantite, 2 pour cacluler l'entropie ou bien arreter avec #: ")
+    if(calcInput == '#'):
+        break
+    if(calcInput == '2'):
+        quantite = 0
+        for i in range(len(probalblities)):
+            quantite += probalblities[i] * math.log2(1/probalblities[i])
+        print("L'entropie est: ", quantite)
+        break
+    else:
+        if(calcInput == '1'):
+            while True:
+                quantElement = input("Entrez l'element dont vous voulez calculer la quantite: ")
+                if(quantElement in source):
+                    index = source.index(quantElement)
+                    print("La quantite de ", quantElement, " est: ", math.log2(1/probalblities[index]))
+                    break
+                else:
+                    print("L'element n'existe pas dans la source. Veuillez réessayer.")
